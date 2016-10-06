5989e24b4b0f998db8236a3fcbff6027089e154f
Darkx1441 <crysist14@gmail.com>
2016-10-06 10:00:29 -0400

    Font,Lines,Star Field
    -new font
    -Corrected xy center line
    -star field random dots

--- src/csit111project/Game.java
+++ src/csit111project/Game.java
@@ -8,7 +8,7 @@ public class Game {
 	public static void main(String[] args) {
 		JFrame window= new JFrame("CALCULATOR IS FOR NOOBS");
 		window.setContentPane(new GamePanel());
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.pack();
 		window.setLocationRelativeTo(null);
--- src/csit111project/GamePanel.java
+++ src/csit111project/GamePanel.java
@@ -4,9 +4,11 @@ import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.geom.Rectangle2D;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
 	//Default serial version ID
@@ -26,12 +28,17 @@ public class GamePanel extends JPanel {
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		g.drawString("HELLO", WIDTH/2, HEIGHT/2); //not accurately centered
+		//g.drawString("HELLO", WIDTH/2, HEIGHT/2); //not accurately centered
 		
 		/*
 		 * Re-drawing accurately centered string
+		 * and graph center lines
 		 */
 		g.setColor(Color.RED);
+		
+		//set font
+		Font myFont=new Font(Font.SANS_SERIF, Font.BOLD, 30);
+		g.setFont(myFont);
 		
 		//get rectangle bounds of text
 		FontMetrics metrics=g.getFontMetrics();
@@ -43,10 +50,32 @@ public class GamePanel extends JPanel {
 		
 		//Calc starting point of centered string
 		int strX=centerPanelX-(int)(strRect.getWidth()/2);
-		int strY=centerPanelY-(int)(strRect.getHeight()/2);
+		int strY=centerPanelY+(int)(strRect.getHeight()/2);
 		
 		//draw centered string
-		g.drawString("HELLO", strX, strY);
+		//g.drawString("HELLO", strX, strY);
+	
+		g.drawLine(centerPanelX, 0, centerPanelX, HEIGHT);
+		g.drawLine(0, centerPanelY, WIDTH, centerPanelY);
+	
+		/*
+		 * Star Field
+		 */
+		
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		Random r= new Random();
+		Color randomColor=new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256));
+		g.setColor(randomColor);
+		for(int i=0;i<3000;i++){
+			int x = Math.abs(r.nextInt()%WIDTH);
+			int y = Math.abs(Math.abs(r.nextInt()%HEIGHT));
+			System.out.println("x: "+x+"/ty:"+y);
+			g.drawLine(x, y, x, y);
+			
+		}
+	
 	}
 	
 
