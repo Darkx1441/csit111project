73cc5382b2f5b9e5280b58611385a4a5fc42aeb3
Darkx1441 <crysist14@gmail.com>
2016-10-07 00:58:52 -0400

    Images, Engine, Resources
    -Calling and drawing images
    -Started working on engine
    -Images and Assets will go into /resources/(?) folder depending on what
    they are(e.g. images, sounds, etc.)

--- src/csit111project/GamePanel.java
+++ src/csit111project/GamePanel.java
@@ -1,5 +1,6 @@
 package csit111project;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import java.awt.Color;
@@ -8,6 +9,8 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Random;
 
 public class GamePanel extends JPanel 
@@ -20,64 +23,54 @@ public class GamePanel extends JPanel
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 480;
 	
+	BufferedImage img;
+	boolean running;
+	
 	public GamePanel()
 	{
 		super();
 		setPreferredSize(new Dimension(WIDTH,HEIGHT));
 		setFocusable(true);
 		requestFocus();
+		init();
 	}
 
+	private void init(){
+		try{
+			img= ImageIO.read(getClass().getResourceAsStream("/images/Splitlevel.png"));
+		} catch (IOException e){
+			e.printStackTrace();
+			}
+	}
+	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		//g.drawString("HELLO", WIDTH/2, HEIGHT/2); //not accurately centered
 		
-		/*
-		 * Re-drawing accurately centered string
-		 * and graph center lines
-		 */
 		g.setColor(Color.RED);
 		
 		//set font
 		Font myFont = new Font(Font.SANS_SERIF, Font.BOLD, 30);
-		g.setFont(myFont);
-		
-		//get rectangle bounds of text
-		FontMetrics metrics = g.getFontMetrics();
-		Rectangle2D strRect = metrics.getStringBounds("HELLO", g);
-		
+		g.setFont(myFont);	
 		//calc center of the screen
 		int centerPanelX = WIDTH / 2;
 		int centerPanelY = HEIGHT / 2;
+
+		if (img!=null){
+			g.drawImage(img, 0, 0, null);
+		}
 		
-		// Calc starting point of centered string
-		int strX = centerPanelX - (int)(strRect.getWidth() / 2);
-		int strY = centerPanelY + (int)(strRect.getHeight() / 2);
 		
-		// draw centered string
-		// g.drawString("HELLO", strX, strY);
-	
+		
+		
+		
+
+		
+		
+		
 		g.drawLine(centerPanelX, 0, centerPanelX, HEIGHT);
 		g.drawLine(0, centerPanelY, WIDTH, centerPanelY);
 	
-		/*
-		 * Star Field
-		 */
 		
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, WIDTH, HEIGHT);
-		
-		Random r = new Random();
-		Color randomColor = new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256));
-		g.setColor(randomColor);
-		
-		for(int i = 0; i < 3000; i++)
-		{
-			int x = Math.abs(r.nextInt() % WIDTH);
-			int y = Math.abs(Math.abs(r.nextInt() % HEIGHT));
-			System.out.println("x: " + x + "/ty:" + y);
-			g.drawLine(x, y, x, y);
-		}
 	}
 }
